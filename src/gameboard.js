8df7167f0ca297d9279572e937ffb50805fb16df
fadelyking <fadely195@gmail.com>
2023-10-19 16:25:50 +0300

    added a function to determine whether an attack is a miss or a hit

--- src/gameboard.js
+++ src/gameboard.js
@@ -15,13 +15,26 @@ function gameBoard(ship, value, location) {
 	// Store the ship in the desired location
 	arrays[value].splice(location, 1, ship);
 
-	// Return true if the ship is there, return false if it isn't
-	checkForShip = (arrayLetter, arrayNumber) => {
+	// Return true if the ship is there, return Miss if it's a miss, and return false if nothing
+	checkForShip = (arrayLetter, arrayIndex) => {
 		const findShip = arrays[arrayLetter];
-		if (findShip[arrayNumber] !== "") return true;
+		if (findShip[arrayIndex] !== "" && findShip[arrayIndex] !== "Miss") {
+			return true;
+		} else if (findShip[arrayIndex] === "Miss") {
+			return "Miss";
+		}
 	};
 
-	receiveAttack = () => {};
+	// Takes a pair of coordinates, determines whether or not the attack hit a ship
+	// If missed, the missed shot is also logged
+	receiveAttack = (arrayLetter, arrayIndex) => {
+		const attackShip = arrays[arrayLetter];
+		if (attackShip[arrayIndex] === "") {
+			return arrays[arrayLetter].splice(arrayIndex, 1, "Miss");
+		} else if (attackShip[arrayIndex] !== "" && attackShip[arrayIndex] !== "Miss") {
+			return attackShip[arrayIndex].name;
+		}
+	};
 
 	return { arrays, checkForShip, receiveAttack };
 }
@@ -29,5 +42,5 @@ function gameBoard(ship, value, location) {
 const newShip = ship(4);
 const newGame = gameBoard(newShip, "A", 1);
 newGame.checkForShip("A", 1);
-
+newGame.receiveAttack("A", 1);
 export { gameBoard };
