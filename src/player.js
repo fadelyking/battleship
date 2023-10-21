c3eefd9d41ed03c101246632b22e7c0d06bfd128
fadelyking <fadely195@gmail.com>
2023-10-21 17:19:02 +0300

    switch players between turns

--- src/player.js
+++ src/player.js
@@ -5,28 +5,30 @@
 // If it's player 2 (computer) turn, randomly choose a place to attack
 import { playerGameBoard, computerGameBoard } from "./gameboard";
 
-function player(player, gameboard) {
+function player(player, gameBoard) {
+	let currentPlayer = player;
 	function randomNum(max) {
 		return Math.floor(Math.random() * max);
 	}
+
 	const attack = () => {
 		if (player === "Human") {
-			gameboard.receiveAttack(2, 3);
+			gameBoard.receiveAttack(2, 3);
 			switchPlayers();
 		} else if (player === "Computer") {
-			gameboard.receiveAttack(randomNum(10), randomNum(10));
+			gameBoard.receiveAttack(randomNum(10), randomNum(10));
 			switchPlayers();
 		}
 	};
 
-	return { attack };
+	return { attack, currentPlayer };
 }
 
 const humanPlayer = player("Human", computerGameBoard);
 const computerPlayer = player("Computer", playerGameBoard);
 
 humanPlayer.attack();
-computerPlayer.attack();
+
 function switchPlayers() {
 	if (humanPlayer) {
 		return computerPlayer;
