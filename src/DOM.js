568b4e7bf6bdbf4c984570236ae9121cfe7b9c7b
fadelyking <fadely195@gmail.com>
2023-10-22 12:37:29 +0300

    added extra styling

--- src/DOM.js
+++ src/DOM.js
@@ -12,6 +12,7 @@ function loadDOM(playerGameBoard, AIGameBoard) {
 	body.appendChild(centerContainer);
 	centerContainer.appendChild(humanContainer);
 	centerContainer.appendChild(computerContainer);
+	const cell = document.createElement("div");
 
 	// Arrays to store the ships in
 	const rows = 10;
@@ -23,7 +24,14 @@ function loadDOM(playerGameBoard, AIGameBoard) {
 		for (let j = 0; j < columns; j++) {
 			const cell = document.createElement("div");
 			cell.classList.toggle(`cell-${i}${j}`);
-			cell.textContent = playerArray[i][j];
+			cell.setAttribute("data", `${playerArray[i][j]}`);
+
+			if (cell.getAttribute("data") === "[object Object]") {
+				cell.style.backgroundColor = "DarkRed";
+			} else if (cell.getAttribute("data") === "Miss") {
+				cell.style.borderRadius = "10px";
+				cell.style.backgroundColor = "Crimson";
+			}
 			humanContainer.appendChild(cell);
 		}
 	}
@@ -34,7 +42,13 @@ function loadDOM(playerGameBoard, AIGameBoard) {
 		for (let j = 0; j < columns; j++) {
 			const cell = document.createElement("div");
 			cell.classList.toggle(`cell-${i}${j}`);
-			cell.textContent = computerArray[i][j];
+			cell.setAttribute("data", `${computerArray[i][j]}`);
+			if (cell.getAttribute("data") === "[object Object]") {
+				cell.style.backgroundColor = "DarkBlue";
+			} else if (cell.getAttribute("data") === "Miss") {
+				cell.style.borderRadius = "10px";
+				cell.style.backgroundColor = "Crimson";
+			}
 			computerContainer.appendChild(cell);
 		}
 	}
