102491f1c89276eb698a96652e08e7c40954685c
fadelyking <fadely195@gmail.com>
2023-10-22 11:18:27 +0300

    fixed up the tests

--- tests/player.test.js
+++ tests/player.test.js
@@ -2,9 +2,8 @@ import { player, switchPlayers } from "../src/player";
 import { playerGameBoard, computerGameBoard } from "../src/gameboard";
 
 test("Players taking turns", () => {
-	const humanPlayer = player("Human", computerGameBoard);
-	const computerPlayer = player("Computer", playerGameBoard);
-	computerPlayer.attack();
-	humanPlayer.attack();
-	expect(humanPlayer.attack()).toBe(computerPlayer.attack());
+	const humanPlayer = player("Human", playerGameBoard, computerGameBoard);
+	const computerPlayer = player("Computer", computerGameBoard, playerGameBoard);
+	computerPlayer.attack(1, 0);
+	expect(humanPlayer.attack(1, 0)).toBe(computerPlayer.attack());
 });
